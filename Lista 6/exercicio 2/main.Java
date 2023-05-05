b0bffc5e3263c7b1846dd48732749690259ebf28
ArceusJKK <120227334+ArceusJKK@users.noreply.github.com>
2023-05-05 18:20:36 -0300

    Create main.Java

---
+++ Lista 6/exercicio 2/main.Java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangulo triangulo = new Triangulo();
+        System.out.println(triangulo.calcularArea(80,60));
+        Quadrado quadrado = new Quadrado();
+        System.out.println(quadrado.calcularArea(50, 60));
+    }
+
+}
